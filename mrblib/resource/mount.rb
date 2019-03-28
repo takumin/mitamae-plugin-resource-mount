f4bc97d863a13c7a5b748bcdc056d74c29d89419
takumin <takumiiinn@gmail.com>
2019-03-28 16:44:51 +0900

    Allows you to select some arguments as needed

--- mrblib/resource/mount.rb
+++ mrblib/resource/mount.rb
@@ -3,10 +3,10 @@ module ::MItamae
     module Resource
       class Mount < ::MItamae::Resource::Base
         define_attribute :action, default: :mount
-        define_attribute :device, type: String, required: true
+        define_attribute :device, type: String, default: ''
         define_attribute :point, type: String, default_name: true
-        define_attribute :type, type: String, required: true
-        define_attribute :options, type: Array, required: true
+        define_attribute :type, type: String, default: ''
+        define_attribute :options, type: Array, default: ['defaults']
         define_attribute :dump, type: Integer, default: 0
         define_attribute :pass, type: Integer, default: 0
         define_attribute :fstab, type: [TrueClass, FalseClass], default: false
